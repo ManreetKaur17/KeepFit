d04d89a9cea00a4e2f65f9a1788567f5faf7396d
Manreet Kaur <mkaur219@my.bcit.ca>
2020-08-15 15:41:35 -0700

    changes to post inspiration

--- scripts/postInspiration.js
+++ scripts/postInspiration.js
@@ -1,16 +1,14 @@
+//===== Adds post to database =====
+
 function postIns() {
-    console.log("Post clicked");
 
     let caption = document.getElementById("inputCaption").value;
-    console.log(caption);
 
     let user = firebase.auth().currentUser.uid;
-    console.log(user);
 
     let selectedFile = document.getElementById("customFile").files[0];
 
     if(typeof(selectedFile)=="undefined") {
-        console.log("Please select picture");
         window.alert("Please select picture");
     } else {
         document.getElementById("postBtn").innerHTML = `
@@ -26,7 +24,6 @@ function postIns() {
         fileRef.put(selectedFile).then(function(snapshot) {
     
             fileRef.getDownloadURL().then(function(url) {
-                console.log("Image url" + url);
                 myUrl = url;
                 document.getElementById("myImage").src = url;
                 console.log("image added successfully");
@@ -38,7 +35,6 @@ function postIns() {
                     user: user
                 })
                 .then(function(docRef) {
-                    console.log("Success in adding document " + docRef);
                     document.getElementById("postBtn").innerHTML = "Post";
                     document.getElementById("myForm").reset();
                     document.getElementById("myImage").src = "../images/imageIcon.png";
@@ -47,12 +43,7 @@ function postIns() {
                 .catch(function(error) {
                     console.log("Error: " + error);
                 })   
-                // TODO: decide where to redirect or not redirect
-                // add another then statement to forward to exercise page
-    
-            /*    .then(function() {
-                    window.location.href = "exercise.html?category=" + `${category}`;
-                })    */
+          
             })
             .catch(function(error) {
                 console.log(error);
@@ -67,6 +58,5 @@ function postIns() {
 }
 
 $('#customFile').on('change', function () {
-    console.log(URL.createObjectURL(event.target.files[0]));
     $('#myImage').attr('src', URL.createObjectURL(event.target.files[0]));
 });
